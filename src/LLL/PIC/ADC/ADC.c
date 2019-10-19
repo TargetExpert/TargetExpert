29484eca68c8d3c3d1499f4d5878cbec6bb0db3d
onting <hemool123@naver.com>
2019-10-20 05:17:20 +0900

    Create ADC files on the PIC

---
+++ src/LLL/PIC/ADC/ADC.c
@@ -0,0 +1,17 @@
+/*********************************************************************
+
+          File: ADC.c
+
+   Description: AVR Analog-to-Digital converter Source Part.
+
+        Author: Kwangyeon Kim (onting, hemool123@naver.com)
+
+       Created: 2019/10/20
+
+ Copyright (c) 2018, TargetExpert Project is Gabriel Kim(Doohoon Kim),
+ All rights reserved.
+
+ ** The "TargetExpert" is distributed under the "3-clause BSD" license.
+    See details COPYING.
+
+*********************************************************************/
---
+++ src/LLL/PIC/ADC/ADC.def.h
@@ -0,0 +1,18 @@
+/*********************************************************************
+
+          File: ADC.def.h
+
+   Description: AVR Analog-to-Digital converter syntax definition
+                Header Part.
+
+        Author: Kwangyeon Kim (onting, hemool123@naver.com)
+
+       Created: 2019/10/20
+
+ Copyright (c) 2018, TargetExpert Project is Gabriel Kim(Doohoon Kim),
+ All rights reserved.
+
+ ** The "TargetExpert" is distributed under the "3-clause BSD" license.
+    See details COPYING.
+
+*********************************************************************/
