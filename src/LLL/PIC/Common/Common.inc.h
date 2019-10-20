78866a54d6ccc37daf25aec40d5713bd449a86c6
onting <hemool123@naver.com>
2019-10-20 10:26:21 +0900

    Write Common.inc.h in PIC

--- src/LLL/PIC/Common/Common.inc.h
+++ src/LLL/PIC/Common/Common.inc.h
@@ -16,7 +16,26 @@
 
 *********************************************************************/
 
+//This source code is written based on SDCC API
+
 #ifndef _Common_inc_h_
 #define _Common_inc_h_
+
+#include <sdcc-lib.h>
+#include <pic14/sdcc-lib.h>
+#include <pic14/pic16fam.h>
+#include <pic14/pic16regs.h>
+
+//define machine feature
+#if defined(__SDCC_PIC16F873A) || \
+    defined(__SDCC_PIC16F876A)
+#define __pic_feature_28_pin__
+
+#elif defined(__SDCC_PIC16F874A) || \
+    defined(__SDCC_PIC16F877A)
+#define __pic_feature_40_44_pin__
+
+#endif //feautre
+
 
 #endif // _Common_inc_h_
