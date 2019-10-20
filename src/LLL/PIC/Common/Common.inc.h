6d725d72c368860e49ca94f3934fdcd8e0676c5d
onting <hemool123@naver.com>
2019-10-20 09:46:43 +0900

    Create Common.inc.h in PIC

---
+++ src/LLL/PIC/Common/Common.inc.h
@@ -0,0 +1,22 @@
+/*********************************************************************
+
+          File: Common.inc.h
+
+   Description: PIC including Common Header Part.
+
+        Author: Kwangyeon Kim (onting, hemool123@naver.com)
+
+       Created: 2019/10/20
+
+ Copyright (c) 2019, TargetExpert Project is Gabriel Kim(Doohoon Kim),
+ All rights reserved.
+
+ ** The "TargetExpert" is distributed under the "3-clause BSD" license.
+    See details COPYING.
+
+*********************************************************************/
+
+#ifndef _Common_inc_h_
+#define _Common_inc_h_
+
+#endif // _Common_inc_h_
