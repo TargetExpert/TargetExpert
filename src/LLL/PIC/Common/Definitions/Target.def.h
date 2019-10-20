2214aa74fafd82dedbafe20ad1711e53697b31f6
Jinseong Jeon <aimer120@nate.com>
2019-10-20 14:48:04 +0000

    Compile bug fix and Target.def.h add

---
+++ src/LLL/PIC/Common/Definitions/Target.def.h
@@ -0,0 +1,28 @@
+/*********************************************************************
+
+          File: TargetDef.h
+
+   Description: Target Definition macro Header Part.
+
+        Author: Jinseong Jeon (aimer120@nate.com)
+
+       Created: 2019/10/20
+
+ Copyright (c) 2018, TargetExpert Project is Gabriel Kim(Doohoon Kim),
+ All rights reserved.
+
+ ** The "TargetExpert" is distributed under the "3-clause BSD" license.
+    See details COPYING.
+
+*********************************************************************/
+
+#ifndef _TARGET_DEF_H_
+#define _TARGET_DEF_H_
+
+#include <pic14/sdcc-lib.h>
+#include <pic14/pic16fam.h>
+#include <pic14/pic16regs.h>
+//TODO: 
+//	pic16 add
+
+#endif // _TARGET_DEF_H_
