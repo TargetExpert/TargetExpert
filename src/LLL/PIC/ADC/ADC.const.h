897330e08e87964cb72d19994a4436979c524195
onting <hemool123@naver.com>
2019-10-20 11:52:18 +0900

    Write ADC.const.h in PIC

--- src/LLL/PIC/ADC/ADC.const.h
+++ src/LLL/PIC/ADC/ADC.const.h
@@ -21,4 +21,28 @@
 
 #include "Common.inc.h"
 
+#define _ADC_MODE_CLOCK_F_DIV_2 0
+#define _ADC_MODE_CLOCK_F_DIV_8 1
+#define _ADC_MODE_CLOCK_F_DIV_32 2
+#define _ADC_MODE_CLOCK_F_RC 3
+#define _ADC_MODE_CLOCK_F_DIV_4 4 
+#define _ADC_MODE_CLOCK_F_DIV_16 5
+#define _ADC_MODE_CLOCK_F_DIV_64 6
+
+#define _ADC_PORT_CONFIG0 0
+#define _ADC_PORT_CONFIG1 1
+#define _ADC_PORT_CONFIG2 2
+#define _ADC_PORT_CONFIG3 3
+#define _ADC_PORT_CONFIG4 4
+#define _ADC_PORT_CONFIG5 5
+#define _ADC_PORT_CONFIG6 6 
+#define _ADC_PORT_CONFIG7 7
+#define _ADC_PORT_CONFIG8 8
+#define _ADC_PORT_CONFIG9 9
+#define _ADC_PORT_CONFIG10 10
+#define _ADC_PORT_CONFIG11 11
+#define _ADC_PORT_CONFIG12 12
+#define _ADC_PORT_CONFIG13 13
+#define _ADC_PORT_CONFIG14 14
+
 #endif //_ADC_const_h_
