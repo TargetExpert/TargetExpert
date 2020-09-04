11e08677b027008b3cfb2d6db09778d0bb89ce0c
MichelleJin12 <53206331+MichelleJin12@users.noreply.github.com>
2020-09-04 17:02:49 +0900

    Create pic16f87xa.h

---
+++ src/LLL/PIC/Common/MachineMap/pic16f87xa.h
@@ -0,0 +1,21 @@
+/*********************************************************************
+          File: pic16f87xa.h
+   Description: PIC pic16f87xa series Machine map Header Part.
+        Author: Seonghee Jin (ganghun7558@hanmail.net) 
+       Created: 2019/10/20
+ Copyright (c) 2018, TargetExpert Project is Gabriel Kim(Doohoon Kim),
+ All rights reserved.
+ ** The "TargetExpert" is distributed under the "3-clause BSD" license.
+    See details COPYING.
+*********************************************************************/
+
+#ifndef _PIC16F87XA_H_
+#define _PIC16F87XA_H_
+
+#include "PinDesc.h"
+
+//TODO:
+// External Interrupt Pins Setting.
+// Pin Change Interrupt Pins Setting.
+
+#endif // _PIC16F87XA_H_
